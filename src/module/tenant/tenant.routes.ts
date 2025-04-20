b23a18a8738cb84806ba0ed897f4509316f1d759
subas-roy <subasroy46@gmail.com>
2025-04-20 14:17:30 +0600

    Merged mohi into development

--- src/module/tenant/tenant.routes.ts
+++ src/module/tenant/tenant.routes.ts
@@ -1,18 +1,17 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { TenantController } from "./tenant.controller";
-import validateRequest from "../../middlewares/validateRequest";
-import { TenantValidationSchema } from "./tenant.validation";
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { TenantController } from './tenant.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { TenantValidationSchema } from './tenant.validation';
 
 const router = express.Router();
 
 router.post(
-  "/requests",
-  auth("tenant"),
+  '/requests',
   validateRequest(TenantValidationSchema.createTenantValidationSchema),
   TenantController.submitRequest
-);
-router.get("/my-requests", auth("tenant"), TenantController.getMyRequests);
-router.put("/profile", auth("tenant"), TenantController.updateTenantProfile);
+); //, auth("tenant")
+router.get('/my-requests', TenantController.getMyRequests); //, auth("tenant")
+router.put('/profile', TenantController.updateTenantProfile); //, auth("tenant")
 
 export const TenantRouter = router;
