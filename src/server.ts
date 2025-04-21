845bd88c303cb0c505e71be18d196b82ff9da934
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-04-22 01:19:38 +0600

    test

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function server() {
     await mongoose.connect(
       "mongodb+srv://basaVara:ek89W9mm2ix9rQhb@cluster0.mh62rbj.mongodb.net/BasaVara?retryWrites=true&w=majority&appName=Cluster0"
     );
-
+// test
     app.listen(5000, () => {
       console.log(`Basa Vara Server is running on port   - Alhamdulillah`);
     });
